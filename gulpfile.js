7c5c80104f59848695061482db2c802b559ec810
Yury <phan@i.ua>
2016-10-11 16:55:03 +0300

    first commit

---
+++ gulpfile.js
@@ -0,0 +1,29 @@
+'use strict';
+
+var gulp = require('gulp'),
+  sass = require('gulp-sass'),
+  concat = require('gulp-concat'),
+  debug = require('gulp-debug'),
+  sourcemaps = require('gulp-sourcemaps'),
+  browserSync = require('browser-sync').create();
+
+gulp.task('sass', function() {
+  return gulp.src('src/main.scss')
+    .pipe(sourcemaps.init())
+    .pipe(sass())
+    .pipe(sourcemaps.write(''))
+    .pipe(gulp.dest('build/css'));
+});
+
+// gulp.watch('src/styles/**', function(event) {
+//        gulp.run('sass');
+//    });
+
+gulp.task('serve', function() {
+  browserSync.init({
+    server: 'build'
+  });
+
+  gulp.watch('src/**', ['sass']);
+  gulp.watch("build/**").on('change', browserSync.reload);
+});
